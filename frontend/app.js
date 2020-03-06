0e977c768b40819ed63c5995905babea6d9bc310
jessekathryn <jessekathrynbarnes@gmail.com>
2020-03-05 22:59:25 -0500

    patch/edit events created in app

--- frontend/app.js
+++ frontend/app.js
@@ -3,7 +3,27 @@ class App {
       document.querySelector('#fields').addEventListener('click', e => {
           const id = parseInt(e.target.dataset.id)
           const field = Field.findById(id);
-          console.log('field');
+          document.querySelector('#update').innerHTML = Field.renderUpdateForm();
+      });
+
+      document.querySelector('#update').addEventListener('submit', e => {
+        e.preventDefault();
+        const id = parseInt(e.target.dataset.id);
+        const field = Field.findById(id);
+        const coordinates = e.target.querySelector('input').value;
+  
+        const bodyJSON = { coordinates };
+        fetch(`http://localhost:3000/api/v1/fields/${field.id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+          body: JSON.stringify(bodyJSON),
+        })
+          .then(res => res.json())
+          // our backend responds with the updated note instance represented as JSON
+          .then(updatedField => console.log(updatedField));
       });
     }
   }
--- frontend/fields.js
+++ frontend/fields.js
@@ -1,16 +1,35 @@
 class Field {
   constructor(data) {
     this.id = data.id;
-    this.name = data.name;
     this.coordinates = data.coordinates;
     Field.all.push(this);
   }
 
-  renderCoordinatesElement() {
+  update({coordinates}){
+    this.coordinates = coordinates;
+  }
+
+  renderListItem() {
     return `
       <p>${this.coordinates}
-        <button data-id=${this.id}>edit</button>
-      </p>`;
+        <button data-id=${this.id}>submit</button>
+      </p>
+      `;
+  }
+
+  static findById() {
+    return this.all.find(field => field.id === id);
+  }
+
+  renderUpdateForm() {
+    return `
+    <form data-id=${this.id}>
+      <p>
+        <input type="text" value="${this.coordinates}" />
+      </p>
+      <button type='submit'>Submit</button>
+    </form>
+    `;
   }
 }
 
