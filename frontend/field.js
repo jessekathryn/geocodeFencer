0ce9517aa67450e75060390a8c6b151ac50c5cb1
jessekathryn <jessekathrynbarnes@gmail.com>
2020-03-12 00:14:54 -0400

    completed front-end design but want to add more graph capabilities

--- frontend/field.js
+++ frontend/field.js
@@ -14,44 +14,44 @@ class Field {
 
   renderFieldItem() {
     return `
-    <div class="card mb-3">
+    <div class="card mb-3 shadow-lg p-3 mb-5 bg-white rounded">
+    <span class="border border-light">
     <div class="google-maps">
         <iframe
             src="https://www.google.com/maps/embed/v1/view?key=AIzaSyDyTi3j0kAbYveFAqOA_pc7NLCMWn4HI6k&center=${this.coordinates}&zoom=18&maptype=satellite"
             width="shrink-to-fit" height="shrink-to-fit">
         </iframe>
-    </div>
-            <div class="card-body">
-            <img src="https://i.ibb.co/LktdsBP/home-icon.png" width="25" height="25"><img
-            src="https://i.ibb.co/C6TgVJJ/cow-copy.png" width="30" height="30"><img
-            src="https://i.ibb.co/QJvmTng/noun-Tractor-1192.png" width="27" height="27"><img
-            src="https://i.ibb.co/Q88xYfd/Monitor.png" width="25" height="25"><img
-            src="https://i.ibb.co/HXW6F3Z/Graph.png" width="25" height="25"><img
-            src="https://i.ibb.co/0MzxB8g/Money.png" width="25" height="25"><img
-            src="https://i.ibb.co/X3Hmrtq/Clipboard.png" width="25" height="25"> 
-            <br>
-            <button type="submit" class="btn btn-secondary my-2"data-id=${this.id}>Create Farm</button>
-      </div>
-    </div>`
+    </div></span>
+    <span class="border border-light">
+            <button type="submit" class="btn btn-secondary my-2"data-id=${this.id}>Create Farm</button><br>
+            <img src="https://i.ibb.co/LktdsBP/home-icon.png" width="50" height="50">
+            <img src="https://i.ibb.co/C6TgVJJ/cow-copy.png" width="60" height="60"><img
+            src="https://i.ibb.co/QJvmTng/noun-Tractor-1192.png" width="52" height="52"><img
+            src="https://i.ibb.co/Q88xYfd/Monitor.png" width="50" height="50"><img
+            src="https://i.ibb.co/HXW6F3Z/Graph.png" width="50" height="50"><img
+            src="https://i.ibb.co/0MzxB8g/Money.png" width="50" height="50"><img
+            src="https://i.ibb.co/X3Hmrtq/Clipboard.png" width="50" height="50">
+            <br>    
+      </span>`
     ;
   }
 
   renderUpdateForm() {
-    return `
-    
-    <div class="card-body">
-        <h5 class="card-title">Building a farm with Cattlelog</h5>
-        <p class="card-text">Add latitude, longitude of your center.</p>
+  return `<div class="shadow-lg p-3 mb-5 bg-info rounded"> 
+  <div class="card-body text-white bg-info border-light "> 
+        <h5 class="card-title">Add latitude, longitude of your center.</h5>
+        <p class="card-text"></p>
         <p>
             <form data-id=${this.id}>
-            <input class="form-control" name="${this.coordinates}" placeholder="32.9924145,-81.016398"/>
-            <button type="submit" class="btn btn-secondary my-2">Reset</button>
+            <input class="form-control-lg" name="${this.coordinates}" placeholder="32.989181, -81.01584" required=""/>
+            <br>
+            <button type="submit" class="btn btn-secondary my-2 small">Save</button>
+            <a href="index.html" class="btn btn-secondary my-2">Reset</a>
             </form>
         </p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
       </div>
     </div>
-    `;
+    </div>`;
   }
 
   static findById(id) {
